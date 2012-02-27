b6358593ac720d16ee6e143dc12bd8acecf16d1c
Dave Herman <dherman@mozilla.com>
2012-02-26 17:53:02 -0800

    added info.rkt

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang setup/infotab
+(define name "array")
+(define blurb
+  (list "Efficient functional arrays."))
+(define primary-file "main.rkt")
+(define categories '(datastructures))
+#;(define scribblings '(("array.scrbl" ())))
+(define release-notes
+  '(p "Initial release."))
+(define repositories '("4.x"))
+(define required-core-version "5.0")
+(define version "1")
